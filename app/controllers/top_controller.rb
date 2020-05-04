525749048bb19ddfe47c338962f8a0e17f749b7f
佐藤　祐弥 <y.satou@satouyuuwatarunoMacBook-Air.local>
2020-05-04 21:50:54 +0900

    Initial Commit Message

---
+++ app/controllers/top_controller.rb
@@ -0,0 +1,5 @@
+class TopController < ApplicationController
+def top
+end
+
+end
---
+++ db/migrate/20200418041020_create_records.rb
@@ -0,0 +1,10 @@
+class CreateRecords < ActiveRecord::Migration[5.2]
+  def change
+    create_table :records do |t|
+      t.string :title
+      t.text :body
+
+      t.timestamps
+    end
+  end
+end
